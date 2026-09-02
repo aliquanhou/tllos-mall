4df55731072a9c2e31d167058b721d4ddaf3da4f
TLLOS Developer <dev@tllos.com>
2026-09-02 15:47:17 +0800

    chore: 彻底删除H5版本，改用PC端响应式自适应Web-1)删除h5/前端项目目录 2)Nginx移除/h5/路由 3)CI/CD移除H5构建步骤 4)PC端已支持Desktop/Mobile响应式自适应

--- h5/src/api/cart.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-export const getCart = () => request({ url: '/cart', method: 'get' })
-export const addToCart = data => request({ url: '/cart', method: 'post', data })
-export const updateCart = (id, data) => request({ url: `/cart/${id}`, method: 'put', data })
-export const deleteCart = id => request({ url: `/cart/${id}`, method: 'delete' })
-export const clearCart = () => request({ url: '/cart/clear', method: 'post' })
-export const selectAllCart = data => request({ url: '/cart/select-all', method: 'post', data })
-export const getCartCount = () => request({ url: '/cart/count', method: 'get' })
--- h5/src/api/home.js
+++
@@ -1,3 +0,0 @@
-import request from '@/utils/request'
-export const getHomeData = () => request({ url: '/home', method: 'get' })
-export const getConfig = () => request({ url: '/config', method: 'get' })
