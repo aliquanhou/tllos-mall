d0e02554df1fc815490fec1ba2b403a1e86f8665
TLLOS Developer <dev@tllos.com>
2026-09-02 14:21:10 +0800

    feat: 商户端页面整合完成-1)删除6组冗余重复文件(login/dashboard/product/order/finance/marketing) 2)新增售后管理页(列表+Tab筛选+审核通过/拒绝+确认收货+详情弹窗) 3)新增商品评价管理页(评价统计+评分筛选+回复评价+隐藏/显示) 4)更新路由配置

--- merchant/src/views/order/list.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-card>
-    <template #header><span>{{ title }}</span></template>
-    <el-empty description="商家端模块开发中..." />
-  </el-card>
-</template>
-<script setup>
-const title = 'order/list'
-</script>
